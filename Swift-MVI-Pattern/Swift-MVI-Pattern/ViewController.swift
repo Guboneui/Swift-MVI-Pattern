6ccd740ebf50fb43eb6328c90a2c03bbd997514c
보니 <73548875+Guboneui@users.noreply.github.com>
2023-07-30 02:18:52 +0900

    [FEAT] Project init Commit

---
+++ Swift-MVI-Pattern/Swift-MVI-Pattern/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Swift-MVI-Pattern
+//
+//  Created by 구본의 on 2023/07/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("🔊[DEBUG]: ViewController didLoad")
+    self.view.backgroundColor = .white
+  }
+}
+
